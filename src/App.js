85ec964c817cb734e2ef12e2bcb0887862b1739a
touraymk <131713898+touraymk@users.noreply.github.com>
2023-09-12 23:32:10 +0200

    Update Dictionaty.js
    - Implemented background image.
    -  Added input - search.
    - Styled Dictionary.js with CSS and Bottstrap.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <div className="container">
+      <div className="container mt-4">
         <Dictionary />
       </div>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Dictionary.css";
+import imageBackground from "./dictionary-bg.jpg";
 
 export default function Dictionary() {
   return (
     <div className="Dictionary">
-      <h1>Dictionary</h1>
+      <img src={imageBackground} className="imageBackground img-fluid" alt="" />
+      <header className="p-4">
+        <h1 className="text-center">Dictionary</h1>
+        <h2>What word would you like to look up</h2>
+      </header>
+      <main>
+        <form>
+          <input
+            type="search"
+            placeholder="Enter word.."
+            className="col-6"
+            autoFocus={true}
+          />
+          <button type="submit">🔍</button>
+        </form>
+      </main>
     </div>
   );
 }
