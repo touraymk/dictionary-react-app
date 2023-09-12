344ece8974ed45aa2957782d79eb2904510423f5
touraymk <131713898+touraymk@users.noreply.github.com>
2023-09-12 22:41:12 +0200

    Added Dictionary.js/css
    Implemented it to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Dictionary />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Dictionary.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  return (
+    <div className="Dictionary">
+      <h1>Dictionary</h1>
+    </div>
+  );
+}
