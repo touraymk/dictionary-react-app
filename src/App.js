f066c59dfb60b22bba38b0f68d4fda60061111d5
touraymk <131713898+touraymk@users.noreply.github.com>
2023-09-17 21:03:14 +0200

    Update App
    - Made App.js/Dictionary.js/DictionaryExplanation.js responsive on smaller devices.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <div className="container mt-4">
+      <div className="container">
         <Dictionary keyword={"serendipity"} />
       </div>
       <footer className="text-center">
